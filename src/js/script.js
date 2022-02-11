7c7ce240cf38f1f0f8427645430a5795d61072a3
Dmitriy H <dmitriy.hdj@yandex.ru>
2022-02-11 18:26:39 +0300

    Added Roboto font using google fonts. A modal window has been added to the main page, scripts have been written to open / close the modal window, as well as automatically open the modal window when scrolling or by time.

---
+++ src/js/script.js
@@ -0,0 +1,46 @@
+'use strict';
+window.addEventListener('DOMContentLoaded', () => {
+    // Modal
+
+    const btnModalOpen = document.querySelector('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    btnModalClose = modal.querySelector('[data-close]'),
+    modalTimerId = setTimeout(openModal, 5000);
+
+    function openModal () {
+    modal.classList.toggle('show');
+    document.body.style.overflow = 'hidden';
+    clearInterval(modalTimerId);
+    }
+
+    function closeModal () {
+    modal.classList.toggle('show');
+    document.body.style.overflow = '';  
+    }
+
+    function showModalByScroll () {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight) {
+    openModal();
+    window.removeEventListener('scroll', showModalByScroll);
+    }
+    }
+
+    btnModalOpen.addEventListener('click', openModal);
+
+    btnModalClose.addEventListener('click', closeModal);
+
+    modal.addEventListener('click', (event) => {
+    if (event.target === modal) {
+    closeModal();       
+    }
+    });
+
+    document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape' && modal.classList.contains('show')) {
+    closeModal();
+    }
+    });
+
+    window.addEventListener('scroll', showModalByScroll);
+
+});
