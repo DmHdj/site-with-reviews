b9a17b88a637e6a28159acc0e0d0bf6922e62ef9
Dmitriy H <dmitriy.hdj@yandex.ru>
2022-02-14 19:24:08 +0300

    Made adaptive layout of the main page. To do this, a hamburger for the mobile version was added to the html. Styles with media queries are moved to a separate css file.

--- src/js/script.js
+++ src/js/script.js
@@ -44,4 +44,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', showModalByScroll);
 
+    // hamburger
+
+    const menu = document.querySelector('.menu_list'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        });
+    });
+
 });
